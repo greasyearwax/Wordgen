58c781e5bbb9b618aa05099929417084eb1e5e07
TheScrawl <thescrawlgaming@gmail.com>
2019-05-10 12:30:07 +1200

    fixed extra b

--- word generator.py
+++ word generator.py
@@ -1,5 +1,5 @@
 import random
-deci = ['a','b','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0',' ']
+deci = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0',' ']
 
 length = int(input("how long would you like the generated word to be?\n"))
 f = open("word.txt","w+")
